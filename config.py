a1f0d7904bc77d885e4ea8f84f8225437b2fea4b
deekon <deekon@dk.dkdoka>
2019-04-03 11:54:21 +0300

    init

---
+++ config.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# Э�~Bо�~B �~Bокен невалидн�~Kй, може�~Bе даже не п�~@обова�~B�~L :)
+token = 'long_string_with_bot_token'
+chat_id = "chat_id_string" # for test
---
+++ torrent_bot.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8
+import config
+import telebot
+from telebot import types
+import time
+import os
+import json
+
+token = config.token
+bot = telebot.TeleBot(token)
+downloan_dir = '/data/Media/Download'
+
+knownUsers = []  # todo: save these in a file,
+userStep = {}  # so they won't reset every time the bot restarts
+
+commands = {  # command description used in the "help" command
+    'start'       : 'Get used to the bot',
+    'help'        : 'Gives you information about the available commands',
+    'sendLongText': 'A test using the \'send_chat_action\' command',
+    'getImage'    : 'A test using multi-stage messages, '
+                        'custom keyboard, and media sending'
+}
+
+imageSelect = types.ReplyKeyboardMarkup(one_time_keyboard=True)  # create the image selection keyboard
+imageSelect.add('cock', 'pussy')
+hideBoard = types.ReplyKeyboardRemove()  # if sent as reply_markup, will hide the keyboard
+
+def get_user_step(uid):
+    if uid in userStep:
+        return userStep[uid]
+    else:
+        knownUsers.append(uid)
+        userStep[uid] = 0
+        print("New user detected, who hasn't used \"/start\" yet")
+        return 0
+
+def listener(messages):
+    """
+    When new messages arrive TeleBot will call this function.
+    """
+    for m in messages:
+        if m.content_type == 'text':
+            # print the sent message to the console
+            print(str(m.chat.first_name) + " [" + str(m.chat.id) + "]: " + m.text)
+
+
+# bot.set_update_listener(listener)  # register listener
+
+'''
+# handle the "/start" command
+@bot.message_handler(commands=['start'])
+def command_start(m):
+    cid = m.chat.id
+    if cid not in knownUsers:  # if user hasn't used the "/start" command yet:
+        knownUsers.append(cid)  # save user id, so you could brodcast messages to all users of this bot later
+        userStep[cid] = 0  # save user id and his current "command level", so he can use the "/getImage" command
+        bot.send_message(cid, "Hello, stranger, let me scan you...")
+        bot.send_message(cid, "Scanning complete, I know you now")
+        command_help(m)  # show the new user the help page
+    else:
+        bot.send_message(cid, "I already know you, no need for me to scan you again!")
+
+
+# help page
+@bot.message_handler(commands=['help'])
+def command_help(m):
+    cid = m.chat.id
+    help_text = "The following commands are available: \n"
+    for key in commands:  # generate help text out of the commands dictionary defined at the top
+        help_text += "/" + key + ": "
+        help_text += commands[key] + "\n"
+    bot.send_message(cid, help_text)  # send the generated help page
+
+
+# chat_action example (not a good one...)
+@bot.message_handler(commands=['sendLongText'])
+def command_long_text(m):
+    cid = m.chat.id
+    bot.send_message(cid, "If you think so...")
+    bot.send_chat_action(cid, 'typing')  # show the bot "typing" (max. 5 secs)
+    time.sleep(3)
+    bot.send_message(cid, ".")
+
+
+# user can chose an image (multi-stage command example)
+@bot.message_handler(commands=['getImage'])
+def command_image(m):
+    cid = m.chat.id
+    bot.send_message(cid, "Please choose your image now", reply_markup=imageSelect)  # show the keyboard
+    userStep[cid] = 1  # set the user to the next step (expecting a reply in the listener now)
+
+
+# if the user has issued the "/getImage" command, process the answer
+@bot.message_handler(func=lambda message: get_user_step(message.chat.id) == 1)
+def msg_image_select(m):
+    cid = m.chat.id
+    text = m.text
+
+    # for some reason the 'upload_photo' status isn't quite working (doesn't show at all)
+    bot.send_chat_action(cid, 'typing')
+
+    if text == "cock":  # send the appropriate image based on the reply to the "/getImage" command
+        bot.send_photo(cid, open('rooster.jpg', 'rb'),
+                       reply_markup=hideBoard)  # send file and hide keyboard, after image is sent
+        userStep[cid] = 0  # reset the users step back to 0
+    elif text == "pussy":
+        bot.send_photo(cid, open('kitten.jpg', 'rb'), reply_markup=hideBoard)
+        userStep[cid] = 0
+    else:
+        bot.send_message(cid, "Don't type bullsh*t, if I give you a predefined keyboard!")
+        bot.send_message(cid, "Please try again")
+
+
+# filter on a specific message
+@bot.message_handler(func=lambda message: message.text == "hi")
+def command_text_hi(m):
+    bot.send_message(m.chat.id, "I love you too!")
+
+
+# default handler for every other text
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def command_default(m):
+    # this is the standard reply to a normal message
+    bot.send_message(m.chat.id, "I don't understand \"" + m.text + "\"\nMaybe try the help page at /help")
+'''
+
+'''
+@bot.message_handler(content_types=['document'])
+def get_file(file):
+    file_info = bot.get_file(file.document.file_id)
+    file_name = file.document.file_name
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open(file_name, 'wb') as new_file:
+        new_file.write(downloaded_file)
+    os.system('mv %s /data/Media/Torrents/' % file_name)
+    print(file_name)
+    if os.path.isfile('/data/Media/Torrents/%s.added' % file_name):
+        print('Загрузка файла %s - Началась!')
+    else:
+        print('ОШИБКА добавления файла %s !!!' % file_name)
+'''
+
+"""
+    chat_id = file.chat.id
+    print(chat_id)
+    file_name = file.document.file_name
+    file_id = file.document.file_id
+    print(file_id)
+    print(file_name)
+
+    url = "https://api.telegram.org/file/%s/getFile?file_id=%s" % (token, file_id)
+    url3 = 'https://api.telegram.org/bot%s/getFile?file_id=%s' % (token, file_id)
+    print(url)
+    print(url3)
+
+    url2 = "https://api.telegram.org/file/%s/%s" % (token, file_name)
+    print(url2)
+"""
+
+if __name__ == '__main__':
+#    bot.polling(none_stop=True)
+    while True:
+        try:
+            bot.polling(none_stop=True)
+        except Exception as e:
+            print(e)  # или п�~@о�~A�~Bо print(e) е�~Aли �~C ва�~A логге�~@а не�~B,
+            # или import traceback; traceback.print_exc() дл�~O пе�~Gа�~Bи полной ин�~D�~K
+            time.sleep(15)
